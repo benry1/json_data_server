9a229a30d596cf4b0ce77403856ef36eae08eab2
benry1 <benhenry12345@gmail.com>
2021-12-31 13:51:25 -0500

    serve files from express

---
+++ assets/js/charts.js
@@ -0,0 +1,101 @@
+var labels = Array()
+var balanceData = Array()
+var rewardsData = Array()
+var ftsoData = Array()
+var rewardEpochs = Array()
+var sgbPriceData = Array()
+var galaBalance = Array()
+var galaPrice = Array()
+
+
+//Returns string of 
+async function fetchBalances() {
+    fetch("http://localhost:3000/getBalanceData").then(resp => buildValues(resp))  
+}
+
+async function buildValues(resp ) {
+    var parsed = await resp.json()
+    console.log(parsed)
+    for (var key in parsed) {
+        if (parsed.hasOwnProperty(key)) {
+            var record = parsed[key]
+            labels.push(record["date"])
+            balanceData.push(record["witterBalance"])
+            rewardsData.push(record["witterReward"])
+            ftsoData.push(record["ftsoReward"])
+            rewardEpochs.push(record["rewardEpoch"])
+            sgbPriceData.push(record["SGBPrice"])
+            galaBalance.push(record["galaBalance"])
+            galaPrice.push(record["galaPrice"])
+        }
+    }
+    renderCharts()
+}
+
+function renderCharts() {
+    const data = {
+        labels: labels,
+        datasets:[{
+            label: 'Songbird Balance',
+            data: balanceData
+        }]
+    };
+    const data1 = {
+        labels: labels,
+        datasets:[{
+            label: 'Songbird Reward',
+            data: rewardsData
+        }]
+    };
+    const data2 = {
+        labels: labels,
+        datasets:[{
+            label: 'FTSO Reward',
+            data: ftsoData
+        }]
+    };
+    const data3 = {
+        labels: labels,
+        datasets:[{
+            label: 'Gala Balance',
+            data: galaBalance
+        }]
+    };
+    const config = {
+        type: 'line',
+        data: data
+    };
+    const config1 = {
+        type: 'line',
+        data: data1,
+    };
+    const config2 = {
+        type: 'line',
+        data: data2,
+    };
+    const config3 = {
+        type: 'line',
+        data: data3,
+    };
+    
+    
+    const myChart = new Chart(
+      document.getElementById('sgbBalance'),
+      config
+    );
+    const myChart1 = new Chart(
+        document.getElementById('sgbReward'),
+        config1
+      );
+      const myChart2 = new Chart(
+        document.getElementById('ftsoFee'),
+        config2
+      );
+      const myChart3 = new Chart(
+        document.getElementById('galaBalance'),
+        config3
+      );
+}
+
+fetchBalances()
+
--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require('express')
 require('dotenv').config()
 const JSONdb = require('simple-json-db');
 const cors = require('cors')
-const getBalance = require("./queryBalances")
+const getBalance = require("./assets/js/queryBalances")
+const path = require('path');
 const app = express()
 const port = 3000
 
 var db = new JSONdb(process.env.db);
 app.use(express.json());
 app.use(cors())
+app.use(express.static(path.join(__dirname)))
 app.options('*', cors())
 
 app.get('/getBalanceData', (req, resp) => {
@@ -26,6 +28,10 @@ app.post('/setBalanceData', (req, resp) => {
         )
 })
 
+app.get('/', (req, resp) => {
+    resp.sendFile(path.join(__dirname, "/balances.html"))
+})
+
 app.listen(port, () => {
-    console.log('Listening on localhost:${port}')
+    console.log('Listening on localhost:', port)
 })
